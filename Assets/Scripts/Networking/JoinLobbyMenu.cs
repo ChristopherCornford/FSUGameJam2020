4051fa30bc023a38bd87b47378d9a9cf9bf0e8f9
Christopher A Cornford <ccornford02906@gmail.com>
2020-10-20 01:09:31 -0400

    Started Networking

---
+++ Assets/Scripts/Networking/JoinLobbyMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class JoinLobbyMenu : MonoBehaviour
+{
+    [SerializeField] private GameNetworkManager networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landPagePanel = null;
+    [SerializeField] private TMP_InputField ipAddressInputField = null;
+    [SerializeField] private Button joinButton = null;
+
+    private void OnEnable()
+    {
+        GameNetworkManager.OnClientConnected += HandleClientConnected;
+        GameNetworkManager.OnClientDisconnected += HandleClientDisconnected;
+    }
+
+    private void OnDisable()
+    {
+        GameNetworkManager.OnClientConnected -= HandleClientConnected;
+        GameNetworkManager.OnClientDisconnected -= HandleClientDisconnected;
+    }
+
+    public void JoinLobby()
+    {
+        string ipAddress = ipAddressInputField.text;
+
+        networkManager.networkAddress = ipAddress;
+        networkManager.StartClient();
+
+        joinButton.interactable = false;
+    }
+
+    private void HandleClientConnected()
+    {
+        joinButton.interactable = true;
+
+        gameObject.SetActive(false);
+        landPagePanel.SetActive(false);
+    }
+
+    private void HandleClientDisconnected()
+    {
+        joinButton.interactable = true;
+    }
+}
---
+++ Assets/Scripts/Networking/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameNetworkManager networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+    [SerializeField] private TextMeshProUGUI hostIPAddressText = null;
+
+    public void HostLobby()
+    {
+        networkManager.StartHost();
+
+        landingPagePanel.SetActive(false);
+
+        hostIPAddressText.text = "IP Address: " + IPManager.GetLocalIPAddress();
+    }
+}
