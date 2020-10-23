ab5f7b340bf7bcc8b81081f6de8bf0f6c7361a26
Christopher A Cornford <ccornford02906@gmail.com>
2020-10-23 17:53:38 -0400

    Starting AI Navigation

---
+++ Assets/Scripts/AI/NavigationTools/WaypointNavagation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointNavagation : MonoBehaviour
+{
+    public Task MyTask { get; private set; }
+
+    [Header("AI Behaviour Information")]
+    [SerializeField] private Task myTask;
+
+
+    private void OnValidate()
+    {
+        if(myTask == MyTask)
+        {
+            return;
+        }
+        else
+        {
+            MyTask = myTask;
+            //Do Stuff
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum Task
+{
+    None = 0,
+    Explore,
+    Patrol,
+    Guard
+}
