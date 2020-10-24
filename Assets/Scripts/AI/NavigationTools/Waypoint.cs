a1be0caa80cccf252eff04030caf7e644137ed6e
Christopher A Cornford <ccornford02906@gmail.com>
2020-10-23 23:49:04 -0400

    Basic Waypoint ai navigation complete

--- Assets/Scripts/AI/NavigationTools/Waypoint.cs
+++ Assets/Scripts/AI/NavigationTools/Waypoint.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    protected float debugDrawRadius = 1.0f;
+    [SerializeField]
+    protected float connectivityRadius = 50f;
+
+    public List<Waypoint> connections;
+    
+    public void OnValidate()
     {
-        
+        this.transform.tag = "Waypoint";
+
+        GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        connections = new List<Waypoint>();
+
+        for (int i = 0; i < allWaypoints.Length; i++)
+        {
+            Waypoint nextWaypoint = allWaypoints[i].GetComponent<Waypoint>();
+
+            if (nextWaypoint != null)
+            {
+                if (Vector3.Distance(this.transform.position, nextWaypoint.transform.position) <= connectivityRadius && nextWaypoint != this)
+                {
+
+                    connections.Add(nextWaypoint);
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrawGizmos()
     {
-        
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, connectivityRadius);
     }
 }
--- Assets/Scripts/AI/NavigationTools/WaypointNavagation.cs
+++ Assets/Scripts/AI/NavigationTools/WaypointNavagation.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class WaypointNavagation : MonoBehaviour
 {
     public Task MyTask { get; private set; }
+    public NavMeshAgent navAgent { get { return this.GetComponent<NavMeshAgent>(); } }
 
     [Header("AI Behaviour Information")]
     [SerializeField] private Task myTask;
+    [SerializeField] private Waypoint startingWaypoint;
 
+    [Header("Current Pathfinding Debug Info")]
+    [SerializeField] private bool reachedDestination;
+
+    [SerializeField] private Vector3 destinationPosition;
+    [SerializeField] private Waypoint destinationWaypoint;
+
+    [SerializeField] private Waypoint previousWaypoint;
 
     private void OnValidate()
     {
@@ -23,16 +33,59 @@ public class WaypointNavagation : MonoBehaviour
             //Do Stuff
         }
     }
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+        AquireNextDestination(startingWaypoint);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AquireNextDestination(Waypoint currentWaypoint)
     {
-        
+        List<Waypoint> possibleDestinations = new List<Waypoint>();
+
+        foreach(Waypoint wp in currentWaypoint.connections)
+        {
+            if(previousWaypoint && wp == previousWaypoint) { continue; }
+
+            else
+            {
+                possibleDestinations.Add(wp);
+            }
+        }
+
+        int randIndex = Random.Range(0, possibleDestinations.Count);
+
+        if (possibleDestinations[randIndex])
+        {
+            destinationWaypoint = possibleDestinations[randIndex];
+            destinationPosition = possibleDestinations[randIndex].transform.position;
+        }
+
+        previousWaypoint = currentWaypoint;
+
+        StartCoroutine(Navigate());
+    }
+
+    public IEnumerator Navigate()
+    {
+        navAgent.SetDestination(destinationPosition);
+
+        reachedDestination = false;
+
+        do
+        {
+            if (navAgent.remainingDistance <= 1.0f)
+                reachedDestination = true;
+            else
+            {
+                Debug.Log(navAgent.name + " is traveling to: " + destinationPosition.ToString() + ". \n Distance Remaining: " + navAgent.remainingDistance.ToString());
+            }
+                yield return new WaitForEndOfFrame();
+
+        }
+        while (!reachedDestination);
+
+        AquireNextDestination(destinationWaypoint);
     }
 }
 
