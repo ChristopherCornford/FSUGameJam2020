39775b3bf089bd5eb351faf2f9e759678a2481dc
Nathaniel Owens <73127612+nw360-R@users.noreply.github.com>
2020-10-26 17:27:51 -0400

    Raycasting Update
    Added item detection and a pop-up to tell the user what an item is. Picking up the item does not exist.

---
+++ Assets/NateRayCast.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class NateRayCast : MonoBehaviour
+{
+    // Component thing to determine how far ahead we can pick shit up.
+    [SerializeField] float PickupRange = 5.0f;
+    //Component thing to make raycasting occur from our player character.
+    public GameObject Body;
+    //Create boolean for whether I can grab an item, as well as finding the text item.
+    public bool canGrabItem;
+    public TextMeshProUGUI pickUpText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Generates an variable for Raycasting data to be stored in.
+        RaycastHit hit;
+        if (Physics.Raycast(Body.transform.position, Body.transform.forward, out hit, PickupRange))
+        {
+            //If the item hit by a raycast has the tag 'Item', it will update the text object with the current text and display said text.
+            if (hit.transform.tag == "Item")
+            {
+                pickUpText.SetText("Press 'E' to pick up " + hit.transform.name + ".");
+                pickUpText.enabled = true;
+            }
+            else
+            {
+                pickUpText.enabled = false;
+            }
+        }
+        else
+        {
+            pickUpText.enabled = false;
+        }
+            
+        }
+
+}
